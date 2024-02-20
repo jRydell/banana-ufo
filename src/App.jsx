6dfde589fb9bc58ac4bd353627c9fb7905e05c60
Johan Rydell <johannilsrydell89@gmail.com>
2024-02-20 16:03:50 +0100

    created UfoList, imported index.css in index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import UfoList from "./Components/UfoList/UfoList.jsx";
+
 
 const App = () => {
-    return <h1>🍌 Hello World! 🛸</h1>;
-}
+    return (
+     <>
+        <h1>🍌 Hello World! 🛸</h1>
+        <UfoList />
+        
+        
+    </>
+    );
+};
 
 export default App;
