bcb4eef09ec5f9814a996875d02ad4b7037803dd
Johan Rydell <johannilsrydell89@gmail.com>
2024-02-20 17:25:03 +0100

    list style

--- src/Components/UfoList/UfoList.jsx
+++ src/Components/UfoList/UfoList.jsx
@@ -1,26 +1,24 @@
-const UfoList = () =>{
+import "./UfoList.css";
+const UfoList = () => {
     return (
-    <>
-    <h2>Ufo list</h2>
-    <ul className="ufolist">
-        <li>Millennium Falcon </li>
-        <li>USS Enterprise </li>
-        <li>TARDIS</li>
-        <li>Serenity</li>
-        <li>Discovery One </li>
-        <li>Nostromo </li>
-        <li>Battlestar Galactica </li>
-        <li>Event Horizon </li>
-        <li>Sulaco</li>
-        <li>Heart of Gold</li>
-    
-    </ul>
-    </>
-    
+        <>
+            <article className='UfoList'>
+                    <h2>Fastest ships in the galaxy!</h2>
+                <ul>
+                    <li>Millennium Falcon</li>
+                    <li>USS Enterprise</li>
+                    <li>TARDIS</li>
+                    <li>Serenity</li>
+                    <li>Discovery One</li>
+                    <li>Nostromo</li>
+                    <li>Battlestar Galactica</li>
+                    <li>Event Horizon</li>
+                    <li>Sulaco</li>
+                    <li>Heart of Gold</li>
+                </ul>
+            </article>
+        </>
     )
-    }
-    
-    
-    
-    
-    export default UfoList;
+}
+
+export default UfoList;
