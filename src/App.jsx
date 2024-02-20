03c81c466174d23536d5eeb3366da1eea8f8e318
Johan Rydell <johannilsrydell89@gmail.com>
2024-02-20 16:17:25 +0100

    Created and Imported Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import UfoList from "./Components/UfoList/UfoList.jsx";
+import Header from "./Components/Header/Header.jsx";
 
 
 const App = () => {
     return (
      <>
-        <h1>🍌 Hello World! 🛸</h1>
+        <Header />
         <UfoList />
+        
         
         
     </>
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <h1>🍌 Hello World! 🛸</h1>
+    </>
+  );
+};
+
+export default Header;
